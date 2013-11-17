1efa7f86824c2a7fc8e216b7a6ff0fcbc6c1336a
Marek Juras <marek.juras@lundegaard.eu>
2013-11-17 09:44:07 +0100

    white spaces

--- src/Bubo/Services/TraverserFactory/TraverserFactory.php
+++ src/Bubo/Services/TraverserFactory/TraverserFactory.php
@@ -9,11 +9,11 @@ namespace Bubo\Services;
 use Bubo;
 
 class TraverserFactory extends BaseService {
-    
+
     private $context;
-    
+
     private $presenter;
-    
+
     public function __construct($context) {
         $this->context = $context;
     }
@@ -21,25 +21,23 @@ class TraverserFactory extends BaseService {
     public function isPresenterSet() {
         return $this->presenter !== NULL;
     }
-    
+
     public function setPresenter($presenter) {
         $this->presenter = $presenter;
     }
-    
+
 
     public function createLabelTraverser($menu) {
         $labelTraverser = new Bubo\Traversing\RenderingTraversers\LabelTraverser($menu);
         return $labelTraverser;
     }
-    
+
     public function createTraverser($class, $menu) {
-        //dump($class);
-        
         $className = "Bubo\Traversing\RenderingTraversers\\" .$class;
         $t = new $className($menu);
-        
+
         return $t;
     }
-    
-    
+
+
 }
